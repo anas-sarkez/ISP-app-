98ff7a478a9954829dd538956cb873ddd43c1f94
Ahmed <ahmedsheb2003@yahoo.com>
2024-06-20 19:21:37 +0200

    implemented sign in screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,12 @@
 import { Stack } from "expo-router";
+import { Appearance } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <Stack>
-        <Stack.Screen
-          name="index"
-          options={{ headerShown: true, title: "login" }}
-        />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </SafeAreaProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./app/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#0F0017",
+        "primary-light": "#C4C4C4",
+        "secondary-dark": "#1D1D2E",
+        "secondary-light": "#E9EFF0",
+        accent: "#C0091E",
+      },
+    },
   },
   plugins: [],
 };
