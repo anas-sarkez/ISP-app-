3ec966129e0d67a37467c6fcdcbf86d99403c234
AbseLY <abdaslam13.2.2001@gmail.com>
2024-07-18 19:03:34 +0200

    FCM features implmented

--- app/(drawer)/(tabs)/home.tsx
+++ app/(drawer)/(tabs)/home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "../../Components/Home";
+
 const home = () => {
   return <Home />;
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,28 @@ import { StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { PaperProvider } from "react-native-paper";
 import { useState } from "react";
+import React, { useEffect } from "react";
+import { Alert } from "react-native";
+import messaging from "@react-native-firebase/messaging";
+import { AppRegistry } from "react-native";
+
 // SplashScreen.preventAutoHideAsync();
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  console.log("Message handled in the background!", remoteMessage);
+});
 
 export default function RootLayout() {
+  const getToken = async () => {
+    const token = await messaging().getToken();
+    console.log("Token :", token);
+  };
+  useEffect(() => {
+    messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+      console.log(typeof remoteMessage);
+    });
+    getToken();
+  }, []);
   const [balance, setBalance] = useState(5);
   const [currentPackage, setCurrentPackage] = useState("Limited3");
 
@@ -34,3 +53,4 @@ export default function RootLayout() {
     </SafeAreaProvider>
   );
 }
+AppRegistry.registerComponent("layout", () => RootLayout);
